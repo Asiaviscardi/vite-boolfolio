d4ae06f2f56668e2c2512e82407207c9ba55dd01
Asiaviscardi <asia.viscardii@gmail.com>
2023-12-14 18:32:00 +0100

    chiamata api e creazione card

--- vite-boolfolio/src/App.vue
+++ vite-boolfolio/src/App.vue
@@ -1,17 +1,31 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted } from 'vue'; 
+import axios from 'axios';
+
+
+const fetchData = async () => {
+  try {
+    const response = await axios.get('https://github.com/Asiaviscardi/laravel-api.git');
+    console.log('Dati ricevuti:', response.data);
+  } catch (error) {
+    console.error('Errore nella chiamata API:', error);
+  }
+};
+
+onMounted(() => {
+  fetchData();
+});
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
+    <a href="https://vitejs.dev/" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
---
+++ vite-boolfolio/src/components/ProjectCard.vue
@@ -0,0 +1,19 @@
+<script>
+
+</script>
+
+<template>
+  <div class="project-card">
+    <h2>card</h2>
+    <p>descrizione</p>
+  </div>
+</template>
+
+<style scoped>
+.project-card {
+  border: 1px solid #ccc;
+  padding: 10px;
+  margin: 10px;
+  background-color: #f9f9f9;
+}
+</style>
